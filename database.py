53f6c2c19182f57b24a1290e9123bab169752bd6
Ezra Muskat <ezramuskat@gmail.com>
2022-06-22 16:18:38 -0400

    updated methods to allow optional testing configuration

--- database.py
+++ database.py
@@ -3,28 +3,38 @@ import os
 import pymysql
 
 
-def add_raw_13f_data_to_database(cik, quarter, data):
+def add_raw_13f_data_to_database(cik, quarter, holdings, test=False):
     """
-    Takes in a cik, quarter, and a set of holdings, and adds the data to the database
+    It takes in a cik, quarter, and list of holdings, and adds them to the database
 
     :param cik: The CIK of the company that the 13F is for
-    :param quarter: The quarter of the 13F filing
-    :param data: The holdings to be added to the database
-    :return: The start of the id for all the holdings added.
+    :param quarter: The quarter of the filing
+    :param holdings: a list of dictionaries, each dictionary representing a holding
+    :param test: a boolean that determines whether or not to use the test database, defaults to False
+    (optional)
+    :return: The id_start variable.
     """
     # load .env data, create the format for the beginning of the id, and set up the format for the queries
     load_dotenv()
     id_start = str(cik) + '-' + str(quarter) + '-'
     sql = "INSERT INTO `raw_13f_data ` (`id`, `cik`, `quarter`, `issuer`, `cusip`, `class`, `value`, `share/prn_amt`, `share/prn_type`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
     # connect to the database
-    connection = pymysql.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME")
-    )
+    if test:
+        connection = pymysql.connect(
+            host=os.environ.get("DB_HOST"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            database=os.environ.get("TEST_DB_NAME")
+        )
+    else:
+        connection = pymysql.connect(
+            host=os.environ.get("DB_HOST"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            database=os.environ.get("DB_NAME")
+        )
     # go through each individual holding
-    for i, holding in enumerate(data):
+    for i, holding in enumerate(holdings):
         # add the holding to the databas
         holding_id = id_start + str(i)
         connection.cursor().execute(sql, (holding_id, cik, quarter, holding["nameOfIssuer"], holding["cusip"], holding[
