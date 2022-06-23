64b385c7fad9c2904976859e3a72a6c21518743e
Ezra Muskat <ezramuskat@gmail.com>
2022-06-23 16:27:18 -0400

    updated database method to conform to the new schema and id generation

--- database.py
+++ database.py
@@ -5,19 +5,17 @@ import pymysql
 
 def add_raw_13f_data_to_database(cik, quarter, holdings):
     """
-    It takes in a cik, quarter, and list of holdings, and adds them to the database
+    Takes in a cik, quarter, and list of holdings, and adds them to the database
 
     :param cik: The CIK of the company that the 13F is for
     :param quarter: The quarter of the filing
     :param holdings: a list of dictionaries, each dictionary representing a holding
-    :param test: a boolean that determines whether or not to use the test database, defaults to False
-    (optional)
-    :return: The id_start variable.
+    :return: The starting sequence for the IDs of all entries added by this method.
     """
     # load .env data, create the format for the beginning of the id, and set up the format for the queries
     load_dotenv()
     id_start = str(cik) + '-' + str(quarter) + '-'
-    sql = "INSERT INTO `raw_13f_data` (`HoldingID`, `cik`, `quarter`, `issuer`, `cusip`, `class`, `value`, `shareprn_amount`, `shareprn_type`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    sql = "INSERT INTO `raw_13f_data` (`HoldingID`, `cik`, `quarter`, `issuer`, `cusip`, `class`, `value`, `shareprn_amount`, `shareprn_type`, `put_call`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     # connect to the database
     connection = pymysql.connect(
         host=os.environ.get("DB_HOST"),
@@ -26,11 +24,15 @@ def add_raw_13f_data_to_database(cik, quarter, holdings):
         database=os.environ.get("DB_NAME")
     )
     # go through each individual holding
-    for i, holding in enumerate(holdings):
-        # add the holding to the databas
-        holding_id = id_start + str(i)
+    for holding in holdings:
+        # add the holding to the database
+        put_call = holding.get("putCall", None)
+        holding_id = id_start + holding["cusip"]
+        if put_call != None:
+            holding_id = holding_id + '-' + put_call
+
         connection.cursor().execute(sql, (holding_id, cik, quarter, holding["nameOfIssuer"], holding["cusip"], holding[
-            "titleOfClass"], holding["value"], holding["shrsOrPrnAmt"]["sshPrnamt"], holding["shrsOrPrnAmt"]["sshPrnamtType"]))
+            "titleOfClass"], holding["value"], holding["shrsOrPrnAmt"]["sshPrnamt"], holding["shrsOrPrnAmt"]["sshPrnamtType"], put_call))
         connection.commit()
     # Returning the id_start variable.
     return id_start
